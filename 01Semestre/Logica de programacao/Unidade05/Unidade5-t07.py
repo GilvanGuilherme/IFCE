c596cd1e1f1ec42a6320f594c99381c90020329f
Gilvan Guilherme <j.gilvan@hotmail.com>
2025-08-27 14:32:05 -0300

    Finalizando unidade 5

--- 01Semestre/Logica de programacao/Unidade05/Unidade5-t07.py
+++ 01Semestre/Logica de programacao/Unidade05/Unidade5-t07.py
@@ -1,1 +1,17 @@
-7 Número de Vogais Peça ao usuário para inserir uma frase. Usando um laço for conte e exiba quantas vogais existem na frase. Lembre de verificar a documentação do módulo String em busca de métodos que facilitem a verificação da letra ser vogal.
+# 7 Número de Vogais Peça ao usuário para inserir uma frase. Usando um laço for conte e exiba quantas vogais existem na frase. Lembre de verificar a documentação do módulo String em busca de métodos que facilitem a verificação da letra ser vogal.
+
+
+while True:
+    frase = input("Digite uma frase: ou sair para ENCERRAR!:")
+    
+    if frase.lower() ==  "sair":
+        print ("Programa Encerrado a'te a proxima")
+        break 
+
+    vogais = "aeiou"
+    contador = 0
+    for letra in frase:
+        if letra.lower() in vogais:
+            contador += 1
+
+    print(f"A frase possui {contador} vogais.")
