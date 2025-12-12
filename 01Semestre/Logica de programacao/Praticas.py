dce389fe8afc475d1b8d5b58e4c15b4c49f370ff
Gilvan Guilherme <gilvanguilherme@Gilvans-MacBook-Pro.local>
2025-12-12 17:57:32 -0300

    Limpeza do repositório: remove pasta duplicada, adiciona gitignore e elimina arquivos do sistema

---
+++ 01Semestre/Logica de programacao/Praticas.py
@@ -0,0 +1,41 @@
+# def somar (a,b):
+#     return a+b
+# resultado=somar(5,8)
+# print (resultado)
+
+
+# entrada= input(int("Digite um numero:"))
+
+# def dobrar(numero):
+#     return numero*2
+# resultado = dobrar(entrada)
+# print(resultado)
+
+# def dobrar(numero):
+#     return numero*2
+# resultado = dobrar(7)
+# print(resultado)
+
+
+# def calcular_media(n1,n2,n3):
+#     return (n1+n2+n3) / 3
+# resultado = calcular_media(8,7,8)
+# print(resultado)
+
+
+# idade=18
+# mensagem = "O individo é maior"
+# def verificar_idade(idade):
+#     if idade >= 18:
+#     return (verificar_idade)
+
+
+def comparar_numero(n1,n2):
+    if n1 > n2:
+        return("O numero 1 é maior")
+    elif n1 < n2:
+        return("O numero 2 é o maior")
+    else:
+        return("Os numeros são iguais")
+        
+print(comparar_numero(11,13))
